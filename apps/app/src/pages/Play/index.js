21861d4aacef7b54d2b9de7ed3598ceb285861a6
Bibek Kumar Bidari <68255317+coder-Bibek@users.noreply.github.com>
2022-06-25 11:45:34 +0545

    Feature/app/logout (#10)
    * feat: add logout for user
    
    * fix: fix css for logout icon
    
    * feat: add make build command
    
    Co-authored-by: bibek@dzangolab <bibek@dzangolab.com>

--- apps/app/src/pages/Play/index.js
+++ apps/app/src/pages/Play/index.js
@@ -1,19 +1,29 @@
+import { toast } from "react-toastify";
 import Button from "../../common/components/atoms/Button";
+import Logout from "../../common/components/atoms/Logout";
 import { Storage } from "../../storage";
 import styles from "./index.module.scss";
 
 export default function Play() {
-    const user = Storage.getItem("user");
+  const user = Storage.getItem("user");
 
-    return (
-        <div className={styles.homeContainer}>
-        <h1 className={styles.header}>Welcome to Tictactoe</h1>
-        <p>Current Player: {user}</p>
-        <div className={styles.playOptions}>
-            <Button title={"Play Single"} isLoading={false} />
-            <Button title={"Host a Room"} isLoading={false} />
-            <Button title={"Join a Room"} isLoading={false} />
-        </div>
+  const removeUser = () => {
+    Storage.removeItem("user");
+    toast("Logout Successfully", { type: "success", autoClose: 2000 });
+  };
+
+  return (
+    <div className={styles.homeContainer}>
+      <div className={styles.logoutContainer}>
+        <Logout onClick={removeUser} />
+      </div>
+      <h1 className={styles.header}>Welcome to Tictactoe</h1>
+      <p>Current Player: {user}</p>
+      <div className={styles.playOptions}>
+        <Button title={"Play Single"} isLoading={false} />
+        <Button title={"Host a Room"} isLoading={false} />
+        <Button title={"Join a Room"} isLoading={false} />
+      </div>
     </div>
-    )
+  );
 }
